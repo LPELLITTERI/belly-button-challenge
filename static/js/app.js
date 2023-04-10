ac96345341477e98f98ccba6bb7239eb5f68d570
lynn <lynn@Admins-MacBook-Pro.local>
2023-04-10 11:53:59 -0400

    built bar chart working on bubble

--- static/js/app.js
+++ static/js/app.js
@@ -28,73 +28,73 @@ d3.json(samples).then(function(data) {
 });
 
 // // Listen for a change event on the dropdown menu
-// dropdownMenu.on('change', function() {
-//   // Get the selected sample ID from the dropdown menu
-//   var selectedSample = d3.select(this).property('value');
-//   // Call the buildBarChart, buildBubbleChart, and showMetadata functions with the selected sample ID
-//   function optionChanged(sample) {
-//     buildBarChart(sample);
-//     buildBubbleChart(sample);
-//     showMetadata(sample);
-//   }
-// });
+dropdownMenu.on('change', function() {
+  // Get the selected sample ID from the dropdown menu
+  let selectedSample = d3.select(this).property('value');
+  // Call the buildBarChart, buildBubbleChart, and showMetadata functions with the selected sample ID
+  function optionchanged(sample) {
+    buildBarChart(sample);
+    buildBubbleChart(sample);
+    showMetadata(sample);
+  }
+});
 
 
-// // Build bar chart
-// function buildBarChart(sample) {
-//   console.log('startingBarChart')
-//   // Get Data
-//   d3.json(samples).then(function(data){
-//     console.log(data);
+ // Build bar chart
+function buildBarChart(sample) {
+  console.log('startingBarChart')
+// Get Data
+d3.json(samples).then(function(data){
+  console.log(data);
 
-// // Use sample_values as the values for the bar chart.
-//     let sampleValues = data.samples;
-//     let sampleArray = sampleValues.filter(sampleObject => sampleObject.id == sample );
-//     let results = sampleArray[0];
-//     let otu_ids = results.otu_ids;
-//     let yTicks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
-//     let x = results.sample_values.slice(0,10).reverse();
-//     // Use otu_ids as the labels for the bar chart.
-//     let text = results.otu_labels.slice(0,10).reverse();
-//     console.log(results);
+// Use sample_values as the values for the bar chart.
+    let sampleValues = data.samples;
+    let sampleArray = sampleValues.filter(sampleObject => sampleObject.id == sample );
+    let results = sampleArray[0];
+    let otu_ids = results.otu_ids;
+    let yTicks = otu_ids.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
+    let x = results.sample_values.slice(0,10).reverse();
+    // Use otu_ids as the labels for the bar chart.
+    let text = results.otu_labels.slice(0,10).reverse();
+    console.log(results);
 
-//     let barData = [ {
-//       y: yTicks,
-//       x: x,
-//       // Use otu_labels as the hovertext for the chart.
-//       hoverText: text,
-//       type: "bar",
-//       orientation: "h",
-//     }]
-//     var layout = {
-//       title: `Top 10 OTUs for Sample ${sample}`,
-//       xaxis: { title: "Sample Values" },
-//       yaxis: { title: "OTU IDs" }
-//     };
-//     Plotly.newPlot("bar", barData, layout);
+    let barData = [ {
+      y: yTicks,
+      x: x,
+      // Use otu_labels as the hovertext for the chart.
+      hoverText: text,
+      type: "bar",
+      orientation: "h",
+    }]
+    var layout = {
+      title: `Top 10 OTUs for Sample ${sample}`,
+      xaxis: { title: "Sample Values" },
+      yaxis: { title: "OTU IDs" }
+    };
+    Plotly.newPlot("bar", barData, layout);
     
 
-//     Plotly.newPlot("bar",barData)
-//   });
-// }
-// function init() {
-//   buildBarChart(940);
-// }
+    Plotly.newPlot("bar",barData)
+  });
+}
+function init() {
+  buildBarChart(940);
+}
 
-// init();
+init();
 
-
-// //Create bubble chart
-// function buildBubbleChart(sample) {
-//   // Use d3.json to fetch the data for the selected sample
-//   d3.json(`/samples/${sample}`).then(function(data) {
-    
-//     // Extract the required data from the JSON response
-//     let xValues = data.otu_ids;
-//     let yValues = data.sample_values;
-//     let markerSizes = data.sample_values;
-//     let markerColors = data.otu_ids;
-//     let textValues = data.otu_labels;
+// Create bubble chart
+function buildBubbleChart(samples) {
+  // Use d3.json to fetch the data for the selected sample
+  d3.json(`/samples/${sample}`).then(function(data) {
+  });
+}
+// Extract the required data from the JSON response
+    let xValues = data.otu_ids;
+    let yValues = data.sample_values;
+    let markerSizes = data.sample_values;
+    let markerColors = data.otu_ids;
+    let textValues = data.otu_labels;
 
 //     // Define the trace for the bubble chart
 //     var trace = {
